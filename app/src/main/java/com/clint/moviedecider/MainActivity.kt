3fa4dcacad37b65d6469848c177c17079c2fb1da
clint <clint@bestdocapp.com>
2021-10-12 22:41:07 +0530

    Added new items in the layout and added a few lines of code in the MainActivity

--- app/src/main/java/com/clint/moviedecider/MainActivity.kt
+++ app/src/main/java/com/clint/moviedecider/MainActivity.kt
@@ -2,10 +2,18 @@ package com.clint.moviedecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clint.moviedecider.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.textViewSelectedMovie.text = "Inception"
     }
 }
